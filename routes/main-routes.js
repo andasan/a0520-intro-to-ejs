44959115d1625e48f26f1d220402174c973126f0
Francois Polo <andasan@gmail.com>
2021-01-07 16:05:41 -0800

    able to read and edit from json file

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,17 +1,47 @@
 const express = require("express");
-
+const fs = require("fs");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("index", {
-      title: "HomePage"
+  let fileExists = fs.existsSync("notes.json");
+  if (fileExists) {
+    fs.readFile("notes.json", (err, data) => {
+      if (err) throw err;
+      return res.render("index", {
+        title: "HomePage",
+        notes: JSON.parse(data),
+      });
     });
+  } else {
+    res.render("index", {
+      title: "HomePage",
+      notes: []
+    });
+  }
 });
 
 router.post("/note", (req, res, next) => {
-    console.log(req.body);
-    res.status(302);
-    res.redirect("/")
+  let data = [];
+  let fileExists = fs.existsSync("notes.json");
+  console.log("does? ", fileExists);
+  if (fileExists) {
+    fs.readFile("notes.json", (err, notesDB) => {
+      if (err) throw err;
+      data = JSON.parse(notesDB);
+      data.push(req.body);
+
+      fs.writeFile("notes.json", JSON.stringify(data), () => {
+        res.status(302);
+        res.redirect("/");
+      });
+    });
+  } else {
+    data.push(req.body);
+    fs.writeFile("notes.json", JSON.stringify(data), () => {
+      res.status(302);
+      res.redirect("/");
+    });
+  }
 });
 
 module.exports = router;
